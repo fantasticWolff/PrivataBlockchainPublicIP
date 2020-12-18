f1b4aac3b99a7cda8d53eabcf06fcad131e6b50e
fantasticWolff <fantastic1936@gmail.com>
2020-12-18 15:48:13 +0800

    finish shell script

--- Install/InstallGeth_source.sh
+++ Install/InstallGeth_source.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+sudo apt install -y vim build-essential make wget
 wget https://storage.googleapis.com/golang/go1.15.linux-amd64.tar.gz
-sudo tar -xvf go1.15.linux-amd64.tar.gz
-sudo mv go /usr/local
+tar -xvf go1.15.linux-amd64.tar.gz
+mv go /usr/local
 export GOROOT=/usr/local/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 export GOPATH=$HOME/go
+rm -rf go1.15.linux-amd64.tar.gz
 go version
